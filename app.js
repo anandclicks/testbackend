20e17fc77fe001754cc7f19228615854faded734
anandclicks <puspanand94@gmail.com>
2025-03-01 17:17:18 +0530

    multer configration

--- app.js
+++ app.js
@@ -14,10 +14,16 @@ app.use('/uploads',express.static(path.join(__dirname, "uploads")))
 const userRoutes = require("./Routes/User.route.js");
 const verifyOtp = require("./helpers/sendOtp.js");
 const getOtp = require("./helpers/generateOtp.js");
+const uploadImage = require("./helpers/multer.js");
 
 //Routes 
 app.use('/v1/users', userRoutes);
-
+app.post('/image',uploadImage.single('profileImage'),(req,res)=> {
+    res.json({
+        message : "Image has uploaded succesfully!",
+        url : `${process.env.BASEURL}uploads/${req.file.filename}`
+    })
+})
 
 // database connecting and starting sever 
 const PORT = process.env.PORT || 3001
---
+++ helpers/multer.js
@@ -0,0 +1,17 @@
+const multer = require('multer');
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination : function(req,res,cb){
+    const destination = path.join("uploads");
+    cb(null,destination)
+  },
+  filename : function(req,res,cb){
+    const fileName = `${Date.now()}.png`
+    cb(null,fileName)
+  }
+})
+
+const uploadImage = multer({storage});
+
+module.exports = uploadImage;
